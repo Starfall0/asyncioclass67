8e87924aee73eda25cc9e7de69ed9b9193730661
Rex <theradeth@gmail.com>
2024-06-26 12:27:56 +0700

    finish assignment2&3

--- assignment3/asyncio01.py
+++ assignment3/asyncio01.py
@@ -1,1 +1,11 @@
 # check the type of a coroutine
+import asyncio
+# define a coroutine
+async def custom_coro():
+    # await another coroutine
+    await asyncio.sleep(1) #sleep แบบไม่ต้องรอฉัน
+
+# create the coroutine
+coro = custom_coro() # return function
+# check the type of the coroutine
+print(type(coro))
--- assignment3/asyncio02.py
+++ assignment3/asyncio02.py
@@ -1,2 +1,14 @@
 # example of running a coroutine
+import asyncio
+# define a coroutine
+async def custom_coro():
+    # await another coroutine
+    await asyncio.sleep(1) #sleep แบบไม่ต้องรอฉัน
 
+# main coroutine
+async def main():
+    #execute my custom coroutine
+    await custom_coro()
+
+#start the coroutine program
+asyncio.run(main())
