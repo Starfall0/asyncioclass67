27dc086df1e90c2c1bda7ee5a5fbf3edeef5a393
Rex <theradeth@gmail.com>
2024-07-10 12:02:13 +0700

    fix assignment5 asyncio05.py

--- assignment5/asyncio05.py
+++ assignment5/asyncio05.py
@@ -4,21 +4,24 @@ import asyncio
 
 async def make_rice():    # fried rice
     value = 1 + random()
+    await asyncio.sleep(value)
     print(f"Microwave (Rice): Cooking {value} seceonds...")
-    await asyncio.sleep(value)  # 2 : pause, another tasks can be run
-    print("Microwave (Rice): Finished cooking")
+    await asyncio.sleep(1)  # 2: pause, another tasks can be run
+    return print("Microwave (Rice): Finished cooking")
 
 async def make_noodle():   # noodle
     value = 1 + random()
+    await asyncio.sleep(value)
     print(f"Microwave (Noodle): Cooking {value} seceonds...")
-    await asyncio.sleep(value)  # 2: pause, another tasks can be run
-    print("Microwave (Noodle): Finished cooking")
+    await asyncio.sleep(1)  # 2: pause, another tasks can be run
+    return print("Microwave (Noodle): Finished cooking")
     
 async def make_curry():   # curry
     value = 1 + random()
+    await asyncio.sleep(value)
     print(f"Microwave (Curry): Cooking {value} seceonds...")
-    await asyncio.sleep(value)  # 2: pause, another tasks can be run
-    print("Microwave (Curry): Finished cooking")
+    await asyncio.sleep(1)  # 2: pause, another tasks can be run
+    return print("Microwave (Curry): Finished cooking")
 
 # main coroutine
 async def main():
@@ -27,11 +30,10 @@ async def main():
     noodle_task = asyncio.create_task(make_noodle(), name = 'Noodle')
     curry_task = asyncio.create_task(make_curry(), name = 'Curry')
     tasks = [rice_task, noodle_task, curry_task]
-    # wait for first tasks to complete
+    # get the first task to complete
     done, pending = await asyncio.wait(tasks, return_when=asyncio.FIRST_COMPLETED)
     # report results
     print(f'Complete task: {len(done)}')
-    # get the first task to complete
     first = done.pop()
     print(f'- {first.get_name()} is completed') # name of task
     print(f'Uncomplete task: {len(pending)}')
